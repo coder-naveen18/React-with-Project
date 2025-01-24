b93a112423100281167caef18c4bd45383b6b497
NAVEEN SAHU <119333274+coder-naveen18@users.noreply.github.com>
2025-01-24 09:41:55 +0000

    Counter Created using the useState hook

---
+++ 03Hooks/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  // let's update the counter using core js
+  // let counter = 5;
+
+  // function addValue(){
+  //   console.log("Clicked" , counter)
+  //   counter = counter + 1;
+
+  // }
+  // The above core js is not able to increment the counter as here react controls the flow and uI updation
+
+
+  // Now use the useState hook --- > it will propagate the values to UI.
+
+  let  [counter, setCounter ] =   useState(5);
+  
+
+  function addValue(){
+    
+    setCounter(counter + 1);
+    
+ 
+  }
+  function removeValue(){
+    
+    setCounter(counter - 1);
+  }
+
+
+  return (
+    <>
+    <h1>chai or code</h1>
+    <h3>Counter Value : {counter}</h3>
+    <button onClick={addValue }>Increment</button>
+    <br />
+    <br />
+    <button onClick={removeValue}>Decrement</button>
+    </>
+  )
+}
+
+export default App
