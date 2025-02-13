f0806a5b10ba4f8e01cdd8db9d00156b34ef857a
NAVEEN SAHU <119333274+coder-naveen18@users.noreply.github.com>
2025-02-13 10:08:56 +0000

    Learned React Router by creating a project

--- React_Router_project_learning/src/App.jsx
+++ React_Router_project_learning/src/App.jsx
@@ -1,10 +1,13 @@
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
 
 import './App.css'
 
 function App() {
   return (
     <>
-    hello
+
     </>
   )
 }
